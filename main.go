b131cde72ea6f3558486461c04c5a70893fb2008
WangZhenyu <wangzhenyu.9264@bytedance.com>
2025-06-13 23:26:38 +0800

    fix: 跨域

--- main.go
+++ main.go
@@ -4,12 +4,27 @@ import (
 	"go-game/repository"
 	"go-game/router"
 
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	repository.InitRedis()
+
 	r := gin.Default()
+
+	// 设置 CORS 中间件，允许所有域名、所有方法、所有 header
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins: true, // 允许所有来源
+		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:    []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:   []string{"Content-Length"},
+		MaxAge:          12 * time.Hour,
+	}))
+
 	router.InitRouter(r)
+
 	r.Run(":8000")
 }
