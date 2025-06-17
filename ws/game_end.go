eeb47da74fb838cf1ec6ad4f0034cab469fd85cc
WangZhenyu <wangzhenyu.9264@bytedance.com>
2025-06-18 00:42:14 +0800

    fix: 游戏结束

--- ws/game_end.go
+++ ws/game_end.go
@@ -1,10 +1,19 @@
 package ws
 
 import (
+	"go-game/dto"
+	"log"
+
 	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/websocket"
 )
 
 func handleGameEndMessage(conn *websocket.Conn, rdb *redis.Client, roomID string, playerID string, msgMap map[string]interface{}) {
+	err := SetGameStatus(rdb, roomID, dto.RoomStatusEnd)
+	if err != nil {
+		log.Println("Error setting game status:", err)
+		return
+	}
+
 	broadcastToRoom(roomID)
 }
