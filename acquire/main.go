9e4b8f4c853a3f10271eefa96a0eb0c78cd96400
WangZhenyu <wangzhenyu.9264@bytedance.com>
2025-07-22 17:27:57 +0800

    fix: 更换域名

--- acquire/main.go
+++ acquire/main.go
@@ -17,7 +17,7 @@ func main() {
 	go ws.ScheduleDailyRoomReset()
 	// 设置 CORS 中间件，允许所有域名、所有方法、所有 header
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://game.yandaifu.xyz", "http://192.168.3.6:3001"}, // 允许你的前端域名跨域访问
+		AllowOrigins:     []string{"http://gamebus.online", "https://gamebus.online", "http://192.168.3.6:3001"}, // 允许你的前端域名跨域访问
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
