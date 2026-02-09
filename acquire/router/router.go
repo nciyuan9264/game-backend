63e891d328472a71084e804129acfbcd52ad48ea
WangZhenyu <wangzhenyu.9264@bytedance.com>
2026-02-09 23:30:04 +0800

    feat: 优化房间锁

--- acquire/router/router.go
+++ acquire/router/router.go
@@ -16,7 +16,7 @@ func InitRouter(r *gin.Engine) {
 	{
 		api.POST("/create", controller.CreateRoom)
 		// api.POST("/delete", controller.DeleteRoom)
-		// api.GET("/list", controller.GetRoomList)
+		api.GET("/list", controller.GetRoomList)
 	}
 
 	// WebSocket 路由
